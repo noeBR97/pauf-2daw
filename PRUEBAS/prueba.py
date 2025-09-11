90a9baf8a5f0f9fdb18e110059d48564350757c1
Noelia Barrionuevo <noe.br97@gmail.com>
2025-09-11 12:00:54 +0200

    repaso sintaxis

--- PRUEBAS/prueba.py
+++ PRUEBAS/prueba.py
@@ -33,3 +33,33 @@ if a > 30:
         print("no eres tan grande")
 else:
     print("adios")
+
+#operadores logicos
+#if x > 10 and x < 100:
+
+#bucles
+contador = 0
+ciclos = ["DAM", "DAW", "ASIR"]
+for ciclo in ciclos:
+    print(ciclo)
+
+for i in range(5, 20):
+    print(i)
+
+while contador > 20:
+    print(contador)
+    contador += 1
+
+lista = ['Marta', 'Víctor', 'Cris', 'Noelia']
+lista.append('Javi')
+lista.remove('Javi')
+
+alumno = lista.pop()#elimina y devuelve el último elemento
+del lista[1]#eliminamos por clave
+print(alumno)
+print(lista)
+lista.insert(2, 'Diego')#insertar en una posición concreta
+print(lista)
+del lista[:2]#elimina elementos desde la posicion 0 hasta la 2
+lista.sort()#ordena alfabéticamente o de menor a mayor
+lista.sort(reverse=True)#ordena al revés
