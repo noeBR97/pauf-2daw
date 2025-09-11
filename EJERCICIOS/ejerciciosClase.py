e58710067c407332cb75aafd9968f228df93c5c1
Noelia Barrionuevo <noe.br97@gmail.com>
2025-09-11 18:11:42 +0200

    fin ejercicios

--- EJERCICIOS/ejerciciosClase.py
+++ EJERCICIOS/ejerciciosClase.py
@@ -6,7 +6,7 @@ listaEquipos.append("Betis")
 listaEquipos.insert(0, "Getafe")
 listaEquipos.remove("Barcelona")
 
-#EJ-2
+# #EJ-2
 locales = ["Real Madrid", "Barcelona", "Atlético", "Sevilla", "Valencia"]
 visitantes = ["Athletic", "Betis", "Cádiz", "Villarreal", "Osasuna"]
 
@@ -20,3 +20,20 @@ partidos.append("Espanyol vs Rayo Vallecano")
 print(partidos)
 
 #EJ-3
+goles = [20, 34, 18, 45, 22, 12]
+print("Hay ", len(goles), " jugadores.")
+
+goles.sort(reverse=True) #ordenados de mayor a menor
+print(goles)
+golesOrdenados = goles.copy()
+golesOrdenados.sort() #ordenados de menos a mayor
+print(goles)
+
+print("Goles máximos: ",max(goles),"\n","Goles mínimos: ",min(goles))
+
+for gol in range(len(golesOrdenados)):
+    if 8 < golesOrdenados[gol]:
+        golesOrdenados.insert(gol,8)
+        break #paramos para que solo lo haga una vez, ya que siempre se cumple la condicion
+
+print(golesOrdenados)
